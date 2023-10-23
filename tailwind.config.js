5babbfcda54d18c9ea6706a8affc750611d6a91f
vinaapatricia <vinaapatricia@gmail.com>
2023-10-23 18:58:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins : ['Poppins', "sans-serif"],
+    },
+    colors: {
+      "kuning" : '#F4CE14',
+      "dark" : '#45474B',
+      "white" : '#F5F7F8'
+    }
+  },
+  plugins: [],
+      }
+
